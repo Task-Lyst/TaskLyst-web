e53822c4b4fd70d41551e3229cea2d282443a3bc
Javier Bonilla <javier.bonilla@psa.es>
2025-07-31 12:11:38 +0200

    Updated GitHub deploy action and vite config to consider custom domain on GitHub pages and local development.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+// Detect environment variables
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
+const isGithubActions = !!process.env.GITHUB_ACTIONS;
+
+// Automatically determine base path:
+// - Local dev → '/'
+// - GitHub Pages → '/<repo-name>/'
+const basePath = isGithubActions ? `/${repoName}/` : '/';
+
 export default defineConfig({
 	plugins: [sveltekit()],
-
+	base: basePath,
+	server: {
+		port: 5173,
+		open: true
+	}
 	css: {
 		preprocessorOptions: {
 			scss: {
