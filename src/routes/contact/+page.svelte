93cf00f024410269065dfc0e4eaf63a20fa109e2
Javier Bonilla <javier.bonilla@psa.es>
2025-07-31 11:27:19 +0200

    Delete unused page.

--- src/routes/contact/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Title from "$lib/components/title.svelte";
-	import Contact from "../../lib/components/contact.svelte";
-</script>
-
-
-<Title title = 'Contact Us' />
-
-<Contact id='contact' />
